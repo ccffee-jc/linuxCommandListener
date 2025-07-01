8bac6fd29389ec4cbfce34d52e60f7f82822cbb0
ccffee-jc <48183006+ccffee-jc@users.noreply.github.com>
2025-07-01 14:46:22 +0800

    Remove hard-coded password and load from property

--- src/main/java/com/ccffee/linuxCommandListener/controller/CommandController.java
+++ src/main/java/com/ccffee/linuxCommandListener/controller/CommandController.java
@@ -2,6 +2,7 @@ package com.ccffee.linuxCommandListener.controller;
 
 import com.ccffee.linuxCommandListener.service.CommandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -12,9 +13,12 @@ public class CommandController {
     @Autowired
     private CommandService commandService;
 
+    @Value("${command.password:}")
+    private String password;
+
     @RequestMapping(value = "/execute", method = RequestMethod.GET)
     public String execute(@RequestParam String command, @RequestParam String pd){
-        if (!pd.equals("qwe123456789")){
+        if (!pd.equals(password)){
             return "you shall not pass!!!";
         }
         return commandService.executeCmd(command);
